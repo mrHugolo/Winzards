de3fba233e9215e46868409ce8f39dbcc2b0c121
Rickard Granath <rickard.granath@live.se>
2021-12-28 16:53:36 +0100

    added rules and made small css changes

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -4,24 +4,39 @@ import css from "../cssModules/Main.module.css"
 import { flipVisibility } from "../utils/Utils"
 
 export const Rules = ({game}) => {
-  const [text, setText] = useState('')
+  const [text, setText] = useState([])
 
   useEffect(() => {
     if(!game?.name) return
     switch(game.name) {
       case "Solitaire": {
-        setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet consectetur adipiscing elit ut. Euismod lacinia at quis risus sed. Quis viverra nibh cras pulvinar mattis nunc sed blandit libero. Vitae congue mauris rhoncus aenean vel elit. Duis ultricies lacus sed turpis. Vel turpis nunc eget lorem dolor sed viverra. Fringilla ut morbi tincidunt augue. Amet purus gravida quis blandit turpis cursus in hac. Fermentum leo vel orci porta non pulvinar. Scelerisque eu ultrices vitae auctor eu augue. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Cras fermentum odio eu feugiat pretium nibh ipsum consequat.")
+        setText(["Gameplay: Move your markers (squares and circles) to create shapes and get points.",
+        "Turn: Make 3 moves in each turn. Before next turn you will be asked to place a new marker on a medioker card.",
+        "Move: In a move you can switch place of two cards, move a pawn or move a marker with the help of a pawn. To move a marker, the pawn, marker and card must have same color.",
+        "Card: The card emotion will give you diffrent options for your move. (While playing, click a card, pawn or marker for options).",
+          "-Powerful(9-11): May switch place with any other empty card. Helps pawn to throw a marker to any empty card. Lets pawn jump to any other empty card.",
+          "-Medioker(4-8): May switch place with an empty neighbour card. Helps pawn to move a marker to an empty neigbour card. Lets pawn move to an empty neigbour card.",
+          "-Sad(1-3): Lets pawn move one step closer to the center."])
         break;
       }
     }
 
   }, [game?.name])
 
+  const boldifyFirstWord = (t) =>{
+    let arr = t.split(":")
+    return <div><span className={css.fontSizeBig}>{arr[0]}</span>:{arr[1]}</div>
+  }
+
   return(
     <div className={css.center}>
       <span className={`${css.pointer} ${css.hide1100}`} onClick={() => flipVisibility("Rules")}>Rules:</span><br/>
       <div className={`Rules ${css.box}`}>
-        <span>{text}</span>
+        <div>
+          {text.length && text.map((t, i) =>(
+            <p className={css.fontSizeSmall} key={i+"r"}>{boldifyFirstWord(t)}</p>
+          ))}
+        </div>
       </div>
     </div>
   )
